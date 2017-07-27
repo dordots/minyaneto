87deb9cdd32fc98ca76a5f0774bd5b7be91f9880
= <mosherk16@gmail.com>
2017-07-27 15:13:46 +0300

    Side bar (with  menu items inside) + Action bar with a "refresh-location" button

--- app/src/main/java/com/app/minyaneto_android/MainActivity.java
+++ app/src/main/java/com/app/minyaneto_android/MainActivity.java
@@ -1,13 +1,84 @@
 package com.app.minyaneto_android;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mToggel;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_LOCALE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        mToolbar = (Toolbar) findViewById(R.id.sidebar_action);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggel = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.action_bar_open, R.string.action_bar_close);
+
+        mDrawerLayout.addDrawerListener(mToggel);
+        mToggel.syncState();
+
+        mNavigationView = (NavigationView) findViewById(R.id.sidebar_navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null){
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggel.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sidebar_home:
+                return true;
+            case R.id.sidebar_settings:
+                return true;
+            case R.id.sidebar_addMinyan:
+                return true;
+            case R.id.sidebar_serach:
+                return true;
+            case R.id.sidebar_about:
+                return true;
+            default:
+                return false;
+        }
     }
 }
--- app/src/main/java/com/app/minyaneto_android/SplashScreen.java
+++ app/src/main/java/com/app/minyaneto_android/SplashScreen.java
@@ -1,31 +1,50 @@
 package com.app.minyaneto_android;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private final int SPLASH_SCREEN_TIME_IN_MILI = 4000;
+    private final int SPLASH_SCREEN_TIME_IN_MILI = 3000;
+    private CountDownTimer splashTimer;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        new Thread(){
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+//        progressBar.setProgressTintList(ColorStateList.valueOf(Color.rgb(230, 0, 172)));
+        progressBar.setProgressTintList(ColorStateList.valueOf(Color.RED));
+        splashTimer = new CountDownTimer(SPLASH_SCREEN_TIME_IN_MILI, 1000) {
             @Override
-            public void run() {
-                try {
-                    sleep(SPLASH_SCREEN_TIME_IN_MILI);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         }.start();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        splashTimer.cancel();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        splashTimer.start();
     }
 }
