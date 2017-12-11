b7039c3092e5cf5f357c9ef1e9a2fbd1325ad496
Ido <idow09@gmail.com>
2017-12-11 20:40:04 +0200

    Removed the mock flavor for reasons of simplicity, Moved mock implementations into the test class (inner). Improved the structure of the build.gradle using version variables.

--- app/src/main/java/com/app/minyaneto_android/location/LocationProvider.java
+++ app/src/main/java/com/app/minyaneto_android/location/LocationProvider.java
@@ -1,11 +1,11 @@
 package com.app.minyaneto_android.location;
 
+import android.location.Location;
+
 import java.util.TimeZone;
 
 public interface LocationProvider {
     TimeZone getTimeZone();
 
-    double getLatitude();
-
-    double getLongitude();
+    Location getLocation();
 }
