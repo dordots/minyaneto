e2905397a30b3be14d7f629298ef7723eccf7929
Ido <idow09@gmail.com>
2017-12-20 02:15:37 +0200

    A whole new updated UI using a new layout based on a RecyclerView. Next tasks: check for Zmanim correctness, add an icon to the nav-drawer, improve tests coverage, and of course add more Zmanim!

--- app/src/main/java/com/app/minyaneto_android/zmanim/ZmanimFragment.java
+++ app/src/main/java/com/app/minyaneto_android/zmanim/ZmanimFragment.java
@@ -1,8 +1,13 @@
 package com.app.minyaneto_android.zmanim;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +23,8 @@ import java.util.Date;
 public class ZmanimFragment extends Fragment implements ZmanimContract.View {
 
     private ZmanimContract.UserActionsListener listener;
-    private TextView alosTv;
-    private TextView misheyakirTv;
-    private TextView tzaisTv;
     private SimpleDateFormat formatter;
+    private ContentAdapter adapter;
 
     public ZmanimFragment() {
         formatter = new SimpleDateFormat("HH:mm");
@@ -44,31 +47,89 @@ public class ZmanimFragment extends Fragment implements ZmanimContract.View {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setupActionBarTitle();
+        return inflateAndSetupRecyclerView(inflater, container);
+    }
 
+    private void setupActionBarTitle() {
         if (getActivity() instanceof MainActivity) {
             ((MainActivity) getActivity())
                     .setActionBarTitle(getResources().getString(R.string.zmanim_fragment));
         }
+    }
 
-        View root = inflater.inflate(R.layout.fragment_zmanim, container, false);
-        alosTv = (TextView) root.findViewById(R.id.alos_hashahar);
-        misheyakirTv = (TextView) root.findViewById(R.id.misheyakir);
-        tzaisTv = (TextView) root.findViewById(R.id.tzais_hakochavim);
-        return root;
+    @NonNull
+    private RecyclerView inflateAndSetupRecyclerView(LayoutInflater inflater, @Nullable ViewGroup container) {
+        RecyclerView recyclerView;
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_zmanim, container, false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new ContentAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        return recyclerView;
     }
 
     @Override
-    public void displayAlosHashahar(Date alos) {
-        alosTv.setText(formatter.format(alos));
+    public void displayAlosHashahar(Date zman) {
+        displayZmanInPosition(zman, 0);
     }
 
     @Override
-    public void displayMisheyakir(Date misheyakir) {
-        misheyakirTv.setText(formatter.format(misheyakir));
+    public void displayMisheyakir(Date zman) {
+        displayZmanInPosition(zman, 1);
     }
 
     @Override
-    public void displayTzaisHakochavim(Date tzais) {
-        tzaisTv.setText(formatter.format(tzais));
+    public void displayTzaisHakochavim(Date zman) {
+        displayZmanInPosition(zman, 2);
     }
+
+    private void displayZmanInPosition(Date zman, int position) {
+        adapter.zmanim[position] = formatter.format(zman);
+        adapter.notifyItemChanged(position);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView zman;
+        public TextView title;
+        public TextView description;
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_zmanim_recycler_view, parent, false));
+            zman = itemView.findViewById(R.id.item_zmanim_zman);
+            title = itemView.findViewById(R.id.item_zmanim_title);
+            description = itemView.findViewById(R.id.item_zmanim_description);
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        private String[] zmanim;
+        private String[] titles;
+        private String[] descriptions;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            descriptions = resources.getStringArray(R.array.zmanim_descriptions);
+            titles = resources.getStringArray(R.array.zmanim_titles);
+            zmanim = new String[titles.length];
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.zman.setText(zmanim[position]);
+            holder.title.setText(titles[position]);
+            holder.description.setText(descriptions[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return zmanim.length;
+        }
+    }
+
 }
