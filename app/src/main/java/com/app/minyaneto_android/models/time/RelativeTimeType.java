dada45580174910dd82221d768210c0e6ed1533a
Ido <idow09@gmail.com>
2018-03-05 21:19:18 +0200

    Removed obsolete code

--- app/src/main/java/com/app/minyaneto_android/models/time/RelativeTimeType.java
+++ app/src/main/java/com/app/minyaneto_android/models/time/RelativeTimeType.java
@@ -1,11 +1,8 @@
 package com.app.minyaneto_android.models.time;
 
 public enum RelativeTimeType {
-    DAWN,
-    SUNRISE,
-    SUNSET,
-    STARS_OUT;
-
-    RelativeTimeType() {
-    }
+  DAWN,
+  SUNRISE,
+  SUNSET,
+  STARS_OUT
 }
